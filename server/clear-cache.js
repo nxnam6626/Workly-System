8b1b6adcca6e0ad95381e4b7f9607f64996202f7
XuanNam <nxnam6626@gmail.com>
2026-04-18 16:08:52 +0700

    xóa các file dư thừa

--- server/clear-cache.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config();
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.aiQueryCache.deleteMany().then(()=>console.log('Cache cleared')).catch(console.error).finally(()=>prisma.$disconnect());
